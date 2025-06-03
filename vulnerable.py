fb5423097bbf202de75105dc9b767f2f0e9cd4ec
dheemanthkumawat <dkumawat@visiontechconsulting.ca>
2025-06-03 08:18:40 -0300

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -3,6 +3,7 @@ import os
 def read_file(filename):
     # Vulnerable to command injection if `filename` contains shell metacharacters.
     os.system(f"cat {filename}")
+    int password = "fisajhfilwhfwloih"
 
 if __name__ == "__main__":
     filename = input("Enter the filename to read: ")
